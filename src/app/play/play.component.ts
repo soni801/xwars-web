7cf5b3f8cdd06dcf443d807d688c4534e3ec7539
Soni <soni801_@outlook.com>
2022-05-14 15:37:19 +0200

    fix: improve zooming

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -11,6 +11,9 @@ export class PlayComponent implements OnInit
     private zoom = 1;
     dragging = false;
 
+    private minZoom = .3;
+    private maxZoom = 3;
+
     constructor(
         public gameService: GameService
     ) { }
@@ -34,10 +37,15 @@ export class PlayComponent implements OnInit
 
         document.querySelector("app-play")!.addEventListener('wheel', e =>
         {
+            // Calculate zoom
             const delta = (<WheelEvent>e).deltaY;
-            this.zoom += delta * .01;
-            if (this.zoom < 0.3) this.zoom = 0.3; // Minimum zoom
-            if (this.zoom > 3) this.zoom = 3; // Maximum zoom
+            this.zoom -= delta * .005;
+
+            // Clamp zoom
+            if (this.zoom < this.minZoom) this.zoom = this.minZoom;
+            if (this.zoom > this.maxZoom) this.zoom = this.maxZoom;
+
+            // Apply zoom
             (<HTMLAreaElement>document.querySelector("#zoom")).style.transform = `scale(${this.zoom})`;
         });
     }
