44ec12229ed27abb569799be3ee56c97990c0dad
Soni <soni@recalstudios.net>
2024-06-09 14:38:59 +0200

    feat: display version

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import packageData from '../../../package.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+    protected version: string = packageData.version;
 }
