ddcb7a17462eddd120825eec06c3ad0596ee9254
Soni <soni@recalstudios.net>
2024-06-09 21:57:43 +0200

    fix: correctly set page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,36 @@ import {AboutComponent} from "./about/about.component";
 import {ChangelogComponent} from "./changelog/changelog.component";
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'play', component: PlayComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'changelog', component: ChangelogComponent },
-    { path: '**', component: NotFoundComponent }
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'The Great X Wars'
+    },
+    {
+        path: 'play',
+        component: PlayComponent,
+        title: 'The Great X Wars'
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About - The Great X Wars'
+    },
+    {
+        path: 'changelog',
+        component: ChangelogComponent,
+        title: 'Changelog - The Great X Wars'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: 'Page not found - The Great X Wars'
+    }
 ];
 
 @NgModule({
